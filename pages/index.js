8b7dc91658de69121d3b7b8f55422ef3d107b53c
Yuriy Bodolanov <magikonermad@yandex.ru>
2022-06-05 15:56:43 +0300

    style: исправить ошибки в оформлении и правки в скрипте js

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,34 @@
-let button = document.querySelector(".profile__edit-btn");
-let buttonClose = document.querySelector(".popup__close-btn");
-let saveButton = document.querySelector(".popup__save-btn");
-let popup = document.querySelector(".popup");
-let profileName = document.querySelector(".profile__name");
-let profileProfession = document.querySelector(".profile__profession");
-let popupName = document.querySelector(".popup__name");
-let popupProfession = document.querySelector(".popup__profession");
-let like = document.querySelectorAll(".element__like");
+let button = document.querySelector('.profile__edit-btn');
+let buttonClose = document.querySelector('.popup__close-btn');
+let saveButton = document.querySelector('.popup__save-btn');
+let popup = document.querySelector('.popup');
+let popupForm = document.querySelector('.popup__inputs');
+let profileName = document.querySelector('.profile__name');
+let profileProfession = document.querySelector('.profile__profession');
+let popupName = document.querySelector('.popup__input_type_name');
+let popupProfession = document.querySelector('.popup__input_type_profession');
 
-popupName.value = profileName.textContent;
-popupProfession.value = profileProfession.textContent;
+function popupOpen() {
+  popup.classList.add('popup_opened');
+  popupName.value = profileName.textContent;
+  popupProfession.value = profileProfession.textContent;
+}
 
-button.addEventListener("click", function () {
-  popup.classList.remove("popup_hidden");
-});
+function popupClose() {
+  popup.classList.remove('popup_opened');
+  // console.log('checked');
+}
 
-buttonClose.addEventListener("click", function () {
-  popup.classList.add("popup_hidden");
-});
+button.addEventListener('click', popupOpen);
 
-popup.addEventListener("click", function (e) {
-  if (e.target === e.currentTarget) {
-    popup.classList.add("popup_hidden");
-  }
-});
+buttonClose.addEventListener('click', popupClose);
 
-let saveBtn = function () {
+function saveBtn() {
+  // debugger;
   profileName.innerText = popupName.value;
   profileProfession.innerText = popupProfession.value;
-};
-
-let saveEnter = function (e) {
-  if (e.code === "Enter") {
-    saveBtn();
-  }
-};
-
-saveButton.addEventListener("click", saveBtn);
-saveButton.addEventListener("keypress", saveEnter);
-
-for (let i = 0; i < like.length; i++) {
-  like[i].addEventListener("click", function () {
-    like[i].style.backgroundImage = "url('../images/like_pressed.svg')";
-  });
+  // console.log('SUBMIT!');
+  popupClose();
 }
+
+popupForm.addEventListener('submit', saveBtn);
